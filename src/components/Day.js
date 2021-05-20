87ea6b1858ca32c0a6386f563912eb289c8de7fd
Khanh.Huynh <khanh.huynh@s3corp.com.vn>
2021-05-20 08:25:38 +0700

    Update Router + comment Call api weather to table

--- src/components/Day.js
+++ src/components/Day.js
@@ -5,9 +5,29 @@ import {
 } from "react-router-dom";
 
 
-export default function Day({ day }) {
+/**
+ * Convert date '2021-05-19' to '2021/5/19' <Link to={`/detail/${locationId}/${convertDateToFlashDate(applicable_date)}`} >
+ * @param {String} dateString 
+ */
+
+const convertDateToFlashDate = (dateString) => {
+    const d = new Date(dateString),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 1)
+      month = '0' + month;
+    if (day.length < 1)
+      day = '0' + day;
+
+    return [year, month, day].join('/');
+}
+console.log(formatDate('2014-12-11'));
+export default Day = ({ day }) => {
+  const { locationId, applicable_date } = day
   return (
-    <Link to="/detail" >
+    <Link to={`/detail/${locationId}/${convertDateToFlashDate(applicable_date)}`} >
       <div className="day" key={day.id}>
         <div className="dayName">{toWeekday(day.applicable_date)}</div>
         <div className="min-temp">{Math.round(day.min_temp)}</div>
--- src/scenes/Detail.js
+++ src/scenes/Detail.js
@@ -1,6 +1,16 @@
+
 import React from 'react';
 
-export default function Detail() {
+export default Detail = () => {
+    // locationID get from url, meterId là tên param, tương ứng với locationId
+
+    // const EditMeter = (props) => {}
+    //     const { match: { params: { meterId } } } = props
+    // datetime
+
+    // call api in useEffect
+
+    // render list of consolidated_weather to table
 
     return (
         <div>
